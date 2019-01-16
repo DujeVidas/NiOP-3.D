4d5d5eed85e5b8a0ad3a8b087a5b409a61342712
DujeVidas <45007960+DujeVidas@users.noreply.github.com>
2019-01-16 17:35:46 +0100

    Add files via upload

---
+++ StreamReaderDujeVidas/StreamReaderDujeVidas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamReaderDujeVidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader nesto = new StreamReader(@"C:\Users\Učenik\Desktop\Yeet.txt");
+            
+            while(!nesto.EndOfStream)
+            {
+                Console.WriteLine(nesto.ReadLine());
+            }
+
+            nesto.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
