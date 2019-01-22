21c676e91d2b3b3ed765d942e9315f9d03c6e304
DujeVidas <45007960+DujeVidas@users.noreply.github.com>
2019-01-22 15:34:55 +0100

    Add files via upload

---
+++ VidasDujeRichTextBoxDZ/VidasDujeRichTextBoxDZ/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace VidasDujeRichTextBoxDZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oProgramuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(63, 32);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 31);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Citaj";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(651, 32);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 31);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Brisi";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(63, 376);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 32);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Spremi";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(63, 69);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(663, 301);
+            this.rtb1.TabIndex = 3;
+            this.rtb1.Text = "";
+            this.rtb1.TextChanged += new System.EventHandler(this.rtb1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oProgramuToolStripMenuItem
+            // 
+            this.oProgramuToolStripMenuItem.Name = "oProgramuToolStripMenuItem";
+            this.oProgramuToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.oProgramuToolStripMenuItem.Text = "O Programu";
+            this.oProgramuToolStripMenuItem.Click += new System.EventHandler(this.oProgramuToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oProgramuToolStripMenuItem;
+    }
+}
+
---
+++ VidasDujeRichTextBoxDZ/VidasDujeRichTextBoxDZ/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VidasDujeRichTextBoxDZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+             rtb1.LoadFile(@"C:\Users\ASUSvivoBook\Desktop\Tempproba.rtf");
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            rtb1.Clear();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+           rtb1.SaveFile(@"C:\Users\ASUSvivoBook\Desktop\Tempproba.rtf");
+            MessageBox.Show("Text Spremljen.");
+        }
+
+        private void rtb1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            O_Programu prog = new O_Programu();
+            prog.Show();
+        }
+    }
+}
