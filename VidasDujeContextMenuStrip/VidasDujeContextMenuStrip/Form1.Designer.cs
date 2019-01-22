a8117f174a5844c38212930fd56d70ddc40cf479
DujeVidas <45007960+DujeVidas@users.noreply.github.com>
2019-01-22 15:55:23 +0100

    Add files via upload

---
+++ VidasDujeContextMenuStrip/VidasDujeContextMenuStrip/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace VidasDujeContextMenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TextLijevo = new System.Windows.Forms.TextBox();
+            this.textDesno = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lijevoNaDesnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desnoNaLijevoCtrlLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oProgramuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextLijevo
+            // 
+            this.TextLijevo.ContextMenuStrip = this.contextMenuStrip1;
+            this.TextLijevo.Location = new System.Drawing.Point(136, 84);
+            this.TextLijevo.Name = "TextLijevo";
+            this.TextLijevo.Size = new System.Drawing.Size(100, 22);
+            this.TextLijevo.TabIndex = 0;
+            this.TextLijevo.TextChanged += new System.EventHandler(this.TextLijevo_TextChanged);
+            // 
+            // textDesno
+            // 
+            this.textDesno.ContextMenuStrip = this.contextMenuStrip1;
+            this.textDesno.Location = new System.Drawing.Point(597, 83);
+            this.textDesno.Name = "textDesno";
+            this.textDesno.Size = new System.Drawing.Size(100, 22);
+            this.textDesno.TabIndex = 1;
+            this.textDesno.TextChanged += new System.EventHandler(this.textDesno_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lijevoNaDesnoToolStripMenuItem,
+            this.desnoNaLijevoCtrlLToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(192, 52);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // lijevoNaDesnoToolStripMenuItem
+            // 
+            this.lijevoNaDesnoToolStripMenuItem.Name = "lijevoNaDesnoToolStripMenuItem";
+            this.lijevoNaDesnoToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
+            this.lijevoNaDesnoToolStripMenuItem.Text = "Lijevo na Desno  ";
+            this.lijevoNaDesnoToolStripMenuItem.Click += new System.EventHandler(this.lijevoNaDesnoToolStripMenuItem_Click);
+            // 
+            // desnoNaLijevoCtrlLToolStripMenuItem
+            // 
+            this.desnoNaLijevoCtrlLToolStripMenuItem.Name = "desnoNaLijevoCtrlLToolStripMenuItem";
+            this.desnoNaLijevoCtrlLToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
+            this.desnoNaLijevoCtrlLToolStripMenuItem.Text = "Desno na Lijevo  ";
+            this.desnoNaLijevoCtrlLToolStripMenuItem.Click += new System.EventHandler(this.desnoNaLijevoCtrlLToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oProgramuToolStripMenuItem
+            // 
+            this.oProgramuToolStripMenuItem.Name = "oProgramuToolStripMenuItem";
+            this.oProgramuToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.oProgramuToolStripMenuItem.Text = "O programu";
+            this.oProgramuToolStripMenuItem.Click += new System.EventHandler(this.oProgramuToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.textDesno);
+            this.Controls.Add(this.TextLijevo);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextLijevo;
+        private System.Windows.Forms.TextBox textDesno;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lijevoNaDesnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desnoNaLijevoCtrlLToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oProgramuToolStripMenuItem;
+    }
+}
+
---
+++ VidasDujeContextMenuStrip/VidasDujeContextMenuStrip/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VidasDujeContextMenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextLijevo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textDesno_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void lijevoNaDesnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textDesno.Text = TextLijevo.Text;
+        }
+
+        private void desnoNaLijevoCtrlLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TextLijevo.Text = textDesno.Text;
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            O_Programu prog = new O_Programu();
+            prog.Show();
+        }
+    }
+}
