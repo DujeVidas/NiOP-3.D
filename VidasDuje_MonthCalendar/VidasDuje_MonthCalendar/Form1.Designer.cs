bfea42b8e70b534ab5e49cbeadfccc3182e93fef
DujeVidas <45007960+DujeVidas@users.noreply.github.com>
2019-01-29 09:55:07 +0100

    Add files via upload

---
+++ VidasDuje_MonthCalendar/VidasDuje_MonthCalendar/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace VidasDuje_MonthCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mC1 = new System.Windows.Forms.MonthCalendar();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oProgramuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mC1
+            // 
+            this.mC1.Location = new System.Drawing.Point(255, 80);
+            this.mC1.MaxSelectionCount = 100;
+            this.mC1.Name = "mC1";
+            this.mC1.TabIndex = 0;
+            this.mC1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.mC1_DateChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(266, 299);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(234, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Odaberite Termin";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oProgramuToolStripMenuItem
+            // 
+            this.oProgramuToolStripMenuItem.Name = "oProgramuToolStripMenuItem";
+            this.oProgramuToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.oProgramuToolStripMenuItem.Text = "O Programu";
+            this.oProgramuToolStripMenuItem.Click += new System.EventHandler(this.oProgramuToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.mC1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar mC1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oProgramuToolStripMenuItem;
+    }
+}
+
---
+++ VidasDuje_MonthCalendar/VidasDuje_MonthCalendar/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VidasDuje_MonthCalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mC1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            TimeSpan ts = mC1.SelectionEnd - mC1.SelectionStart;
+
+            if (ts.Days < 1)
+            {
+                MessageBox.Show("Korištenjem tipke SHIFT odaberite opseg datuma.");
+                return;
+            }
+
+            MessageBox.Show("Rezervirali ste usluge u trajanju od " + ts.Days.ToString() + " dana.", "Rezervacija");
+            MessageBox.Show("Vasa rezervacija počinje " + mC1.SelectionStart.ToShortDateString() + " a završava se " + mC1.SelectionEnd.ToShortDateString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            O_Programu prg = new O_Programu();
+            prg.Show();
+        }
+    }
+}
