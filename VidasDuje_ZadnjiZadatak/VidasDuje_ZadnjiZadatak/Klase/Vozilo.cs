1ab02f830d69119b94089115a7c386da78a557f2
DujeVidas <45007960+DujeVidas@users.noreply.github.com>
2019-02-27 17:59:34 +0100

    Add files via upload

---
+++ VidasDuje_ZadnjiZadatak/VidasDuje_ZadnjiZadatak/Klase/Vozilo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidasDuje_ZadnjiZadatak.Klase
+{
+    class Vozilo
+    {
+        string vrsta;
+        double MaxV;
+        int brojKotaca;
+
+
+        public void setBrojKotaca(int brojKotaca)
+        {
+            this.brojKotaca = brojKotaca;
+        }
+        public int getBrojKotaca()
+        {
+            return this.brojKotaca;
+        }
+        public void setMaxV(double MaxV)
+        {
+            this.MaxV = MaxV;
+        }
+        public double getMaxV()
+        {
+            return this.MaxV;
+        }
+        public void setVrsta(string vrsta)
+        {
+            this.vrsta = vrsta;
+        }
+        public string getVrsta()
+        {
+            return this.vrsta;
+        }
+
+        public Vozilo(int brojKotaca, string vrsta, int MaxV)
+        {
+            this.brojKotaca = brojKotaca;
+            this.vrsta = vrsta;
+            this.MaxV = MaxV;
+        }
+
+        public Vozilo()
+        {
+
+        }
+        public override string ToString()
+        {
+
+            return " Vrsta: " + vrsta  + "\n Broj Kotaca: " + brojKotaca + "\n Maksimalna brzina: " + MaxV;
+        }
+    }
+}
---
+++ VidasDuje_ZadnjiZadatak/VidasDuje_ZadnjiZadatak/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidasDuje_ZadnjiZadatak.Klase;
+
+namespace VidasDuje_ZadnjiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Vozilo Motor = new Vozilo();
+            Motor.setVrsta("Motor");
+            Motor.setBrojKotaca(2);
+            Console.Write(" \n Upisite maksimalnu brzinu Motocikla: ");
+            Motor.setMaxV(Convert.ToInt32(Console.ReadLine()));
+
+            Vozilo Auto = new Vozilo();
+            Auto.setVrsta("Automobil");
+            Auto.setBrojKotaca(4);
+            Auto.setMaxV(Motor.getMaxV()*1.30);
+
+            Console.WriteLine(Motor.ToString());
+            Console.WriteLine(Auto.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
